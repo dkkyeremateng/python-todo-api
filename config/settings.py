3c3d9228547ab7cfcdb4b2a2c85a2f0dccea0c82
Daniel Kofi Kyeremateng <wayne@Daniels-iMac.local>
2017-09-19 15:26:40 +0000

    server_name

--- config/settings.py
+++ config/settings.py
@@ -1,14 +1,3 @@
 # Flask Configs
 DEBUG = True
 SECRET_KEY = 'mysecretkey'
-SERVER_NAME = 'python-keep-api.herokuapp.com'
-
-# Setting posts per page for pagination
-POSTS_PER_PAGE = 5
-
-HOST = 'mongodb://localdev:devpassword@ds141264.mlab.com:41264/keep-api'
-# MONGODB_HOST = 'mongodb'  # use 'mongodb' if using Docker
-# DB = 'keepAPI'
-MONGODB_SETTINGS = {
-    'host': HOST
-}
